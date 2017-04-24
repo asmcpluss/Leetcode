b73aa8ac364a21093b7bfb13fbe1e9162243633c
Dell <1223484098@qq.com>
2017-04-24 15:40:30 +0800

    add operation of KMP algorithm

--- src/test/KMP.java
+++ src/test/KMP.java
@@ -1,22 +1,58 @@
 package test;
 
+import java.util.ArrayList;
+
 public class KMP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String oriStr;
-		String para="";
-		int next[] = computeNext(para);
+		String str = "babsdfaadsdddljl";
+		String pattern="dlj";
+		ArrayList<Integer> result = matchString(str,pattern);
+		if(result.size()==0){
+			System.out.println(-1);
+		}
+		for(int i=0;i<result.size();i++){
+			System.out.print(result.get(i)+" ");
+		}
 
+	}
+	public static ArrayList<Integer> matchString(String str,String subStr){
+		ArrayList<Integer> appearPos = new ArrayList<Integer>();
+		int next[] = computeNext(subStr);
+		int n = str.length();
+		int m = subStr.length();
+		int pOfSub = 0;
+		for(int i=0;i<n;i++){
+			while(pOfSub>0 && subStr.charAt(pOfSub)!=str.charAt(i)){
+				pOfSub = next[pOfSub-1];
+			}
+			if(str.charAt(i)==subStr.charAt(pOfSub)){
+				pOfSub += 1;
+			}
+			if(pOfSub==m){ //find a successful match and start a new match
+				appearPos.add(i-m+1);
+				pOfSub = next[pOfSub-1];
+			}
+		}
+		return appearPos;
 	}
 
 	public static int[] computeNext(String para){
 		int length = para.length();
 		int next[] = new int[length];
+		next[0] = 0;
+		next[1] = 0;
 		String subStr = para;
-		
-		for(int i=0;i<length;i++){
-			
+		int match_position = 0;
+		for(int i=1;i<length;i++){
+			while(match_position>0 && para.charAt(match_position)!=para.charAt(i)){
+				match_position = next[match_position-1];
+			}
+			if(para.charAt(match_position)==para.charAt(i)){
+				match_position += 1;
+			}
+			next[i] = match_position;
 		}
 		
 		return next;
